9971aa8fa83a986a6dbf3d2cb12958a81607cff4
Mary Maguire <mary.maguire@bottlerocketapps.com>
2016-05-02 08:16:49 -0500

    I had cleared out the code that handles serial port communication

--- ukulele/teensy/teensy.ino
+++ ukulele/teensy/teensy.ino
@@ -100,9 +100,7 @@ void loop() {
     // Restart audio sampling.
     samplingBegin();
   }
-    
-  // Parse any pending commands.
-  parserLoop();
+   
 }
 
 
@@ -244,79 +242,3 @@ boolean samplingIsDone() {
   return sampleCounter >= FFT_SIZE*2;
 }
 
-
-////////////////////////////////////////////////////////////////////////////////
-// COMMAND PARSING FUNCTIONS
-// These functions allow parsing simple commands input on the serial port.
-// Commands allow reading and writing variables that control the device.
-//
-// All commands must end with a semicolon character.
-// 
-// Example commands are:
-// GET SAMPLE_RATE_HZ;
-// - Get the sample rate of the device.
-// SET SAMPLE_RATE_HZ 400;
-// - Set the sample rate of the device to 400 hertz.
-// 
-////////////////////////////////////////////////////////////////////////////////
-
-void parserLoop() {
-  // Process any incoming characters from the serial port
-  while (Serial.available() > 0) {
-    char c = Serial.read();
-    // Add any characters that aren't the end of a command (semicolon) to the input buffer.
-    if (c != ';') {
-      c = toupper(c);
-      strncat(commandBuffer, &c, 1);
-    }
-    else
-    {
-      // Parse the command because an end of command token was encountered.
-      parseCommand(commandBuffer);
-      // Clear the input buffer
-      memset(commandBuffer, 0, sizeof(commandBuffer));
-    }
-  }
-}
-
-// Macro used in parseCommand function to simplify parsing get and set commands for a variable
-#define GET_AND_SET(variableName) \
-  else if (strcmp(command, "GET " #variableName) == 0) { \
-    Serial.println(variableName); \
-  } \
-  else if (strstr(command, "SET " #variableName " ") != NULL) { \
-    variableName = (typeof(variableName)) atof(command+(sizeof("SET " #variableName " ")-1)); \
-  }
-
-void parseCommand(char* command) {
-  if (strcmp(command, "GET MAGNITUDES") == 0) {
-    for (int i = 0; i < FFT_SIZE; ++i) {
-      Serial.println(magnitudes[i]);
-    }
-  }
-  else if (strcmp(command, "GET SAMPLES") == 0) {
-    for (int i = 0; i < FFT_SIZE*2; i+=2) {
-      Serial.println(samples[i]);
-    }
-  }
-  else if (strcmp(command, "GET FFT_SIZE") == 0) {
-    Serial.println(FFT_SIZE);
-  }
-  GET_AND_SET(SAMPLE_RATE_HZ)
-  GET_AND_SET(LEDS_ENABLED)
-  GET_AND_SET(SPECTRUM_MIN_DB)
-  GET_AND_SET(SPECTRUM_MAX_DB)
-  
-  // Update spectrum display values if sample rate was changed.
-  if (strstr(command, "SET SAMPLE_RATE_HZ ") != NULL) {
-    spectrumSetup();
-  }
-  
-  // Turn off the LEDs if the state changed.
-  if (LEDS_ENABLED == 0) {
-    for (int i = 0; i < NEO_PIXEL_COUNT; ++i) {
-      pixels.setPixelColor(i, 0);
-    }
-    pixels.show();
-  }
-}
